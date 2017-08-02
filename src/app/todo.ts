454e79164f00b5ae37a0d1b24f7c13c6f0a40690
saini-saivarun <saivarun.saini@gmail.com>
2017-08-02 01:28:35 -0500

    Add files via upload

---
+++ src/app/todo.ts
@@ -0,0 +1,17 @@
+export class Todo {
+  id: number;
+  title: string = '';
+  complete: boolean = false;
+
+  constructor(values: Object = {}) {
+    Object.assign(this, values);
+  }
+}
+
+
+
+
+
+/*
+let todo = new Todo({ title: 'Read SitePoint article', complete: false });
+*/
